b0425317bfe55aba21462b6e5a7bd3f5c81ec5fe
exkuretrol <33695301+exkuretrol@users.noreply.github.com>
2021-08-14 00:31:51 +0800

    Prevent  superfluous commands with conditionals

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -2,11 +2,19 @@
 
 echo "\n<<< Starting ZSH Setup >>>\n"
 
-echo "Enter superuser (sudo) password to edit /etc/shells"
-echo "/opt/homebrew/bin/zsh" | sudo tee -a "/etc/shells" > /dev/null
+if grep -qx "/opt/homebrew/bin/zsh" "/etc/shells"; then
+  echo "/opt/homebrew/bin/zsh already exists in /etc/shells"
+else
+  echo "Enter superuser (sudo) password to edit /etc/shells"
+  echo "/opt/homebrew/bin/zsh" | sudo tee -a "/etc/shells" > /dev/null
+fi
 
-echo "Enter user password to change default login  shell"
-chsh -s "/opt/homebrew/bin/zsh"
+if [ "$SHELL" = "/opt/homebrew/bin/zsh" ]; then
+  echo "\$SHELL is already /opt/homebrew/bin/zsh"
+else
+  echo "Enter user password to change default login shell"
+  chsh -s "/opt/homebrew/bin/zsh"
+fi
 
 if sh --version | grep zsh > /dev/null; then
   echo "/private/var/select/sh has been linked to /bin/zsh"
