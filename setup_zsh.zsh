4d71258ffb0dc749d2a1daa825a04893425e34bc
exkuretrol <33695301+exkuretrol@users.noreply.github.com>
2021-08-14 00:31:02 +0800

    symlink to zsh

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -7,3 +7,10 @@ echo "/opt/homebrew/bin/zsh" | sudo tee -a "/etc/shells" > /dev/null
 
 echo "Enter user password to change default login  shell"
 chsh -s "/opt/homebrew/bin/zsh"
+
+if sh --version | grep zsh > /dev/null; then
+  echo "/private/var/select/sh has been linked to /bin/zsh"
+else
+  echo "Enter superuser (sudo) password to change sh to zsh"
+  sudo ln -fvs /bin/zsh /private/var/select/sh
+fi
