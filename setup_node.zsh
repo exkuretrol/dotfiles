2fe910cc6d0eda94b2231fb639760f09363815ad
exkuretrol <33695301+exkuretrol@users.noreply.github.com>
2021-08-14 03:15:29 +0800

    Move N_PREFIX to the start of PATH
    Yarn requires Node as a dependency, so we will force to install it with HomeBrew. But we want to use the version(s) installed with n. Putting N_PREFIX at the start of PATH means we can prefer items installed via NPM over Homebrew.
    
    
    Just change the PATH order 🤦‍♂️

--- setup_node.zsh
+++ setup_node.zsh
@@ -2,20 +2,6 @@
 
 echo "\n<<< Starting Node Setup >>>\n"
 
-if command -v node | grep -q /opt/homebrew/bin/node; then
-  # If node (Installed by brew) didn't removed, it 
-  # will conflicting with node (Installed by n), because 
-  # the priority of brew-installed node always higher 
-  # than n-installed node.
-  # https://stackoverflow.com/a/26919540
-  # TODO: remove this in the future.
-  echo "Removing latest node dependencied by prettier with brew..."
-  brew uninstall --ignore-dependencies node
-  rm -f /opt/homebrew/bin/npm
-  rm -rf /opt/homebrew/lib/node_modules/
-  brew cleanup
-fi
-
 if exists node; then
   echo "Node $(node --version) & NPM $(npm --version) already installed"
 else
@@ -24,6 +10,7 @@ else
 fi
 
 # Install Global NPM Packages
+npm install --global yarn
 npm install --global firebase-tools
 npm install --global @angular/cli
 npm install --global @ionic/cli
