f63cb7a6e8970119bb5a1708f6fc747d10ce75cf
exkuretrol <33695301+exkuretrol@users.noreply.github.com>
2021-08-14 02:32:07 +0800

    Install some global npm packages
    Firebase, Angular, Typescript, and json / http servers.

--- setup_node.zsh
+++ setup_node.zsh
@@ -22,3 +22,15 @@ else
   echo "Installing Node & NPM with n..."
   n latest
 fi
+
+# Install Global NPM Packages
+npm install --global firebase-tools
+npm install --global @angular/cli
+npm install --global @ionic/cli
+npm install --global typescript
+npm install --global json-server
+npm install --global http-server
+
+echo "Global NPM Packages installed:"
+npm list --global --depth=0
+ 
